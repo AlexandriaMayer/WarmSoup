d26d25881147b1a346491eef9b57dd97288e02d0
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-02-11 01:28:58 +0000

    started working with beautiful soup

--- NameGrab.py
+++ NameGrab.py
@@ -4,3 +4,10 @@ import datetime
 from bs4 import BeautifulSoup
 import requests
 
+url = 'https://www.behindthename.com/top/lists/united-states/1992'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table', attrs = {'class':'r1r2 pop-table pop-table-m'})
+table_rows = table.find_all('tr')
