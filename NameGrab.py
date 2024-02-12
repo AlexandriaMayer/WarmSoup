af650ca05b45d42e633ae8a7013c7fbd2c41324a
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-02-12 04:59:23 +0000

    Updated parser

--- NameGrab.py
+++ NameGrab.py
@@ -9,5 +9,18 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
+
+
 table = soup.find('table', attrs = {'class':'r1r2 pop-table pop-table-m'})
 table_rows = table.find_all('tr')
+# tr means table code
+rows_data = []
+headers = [header.text for header in table_rows[0].find_all('th')]
+
+for m_table in table_rows[1:]:
+    name_table = m_table.find_all('td')
+    name_data = {headers[i]: name_table.text for i, name_table in enumerate(name_table)}
+    rows_data.append(name_data)
+    print(rows_data)
+
+    # this is not working, will have to try again tomorrow.
