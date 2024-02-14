efa2c2bd916a944cb9f1ab0366c465bab42e6eb4
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-02-13 23:20:08 -0600

    Update NamePrint.py

--- NamePrint.py
+++ NamePrint.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import re
 
-name = input('What is your name? \n')
+f_name = input('First Name \n')
+m_name = input('Middle Name \n')
+l_name = input('Last Name \n')
 
 #pattern = r'^(\w).*(\s+\w+)$'
 #initals = re.search(pattern, name)
 #print((initals.group(1)+initals.group(2)+ " ") * 100)
-f_i1, m_i1, l_i1 = name.split (' ', 2)
 pattern = r'^(\w).*(\s+\w+)$'
-f_i2 = re.search(pattern, f_i1)
-m_i2 = re.search(pattern, m_i1)
-l_i2 = re.search(pattern, l_i1)
-print(f_i2+m_i2+l_i2+" "* 100)
+fi = re.search(pattern, f_name)
+mi = re.search(pattern, m_name)
+li = re.search(pattern, l_name)
+
+print(fi+mi+li+" "* 100)
 
 # this is printing out the first inital, but returns the full name of the other string.
 # Maybe I should split the string by spaces.
