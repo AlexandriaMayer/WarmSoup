efb7d42a613e72bc3b62078b3bbdf1d26fe29050
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-02-14 04:49:04 +0000

    Today, I am reminded why almost every name entry form has seperate forms for first, middle and last name. Regex is not infinitely powerful.

--- NamePrint.py
+++ NamePrint.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import re
 
 name = input('What is your name? \n')
-print((name + " ") * 100)
 
+#pattern = r'^(\w).*(\s+\w+)$'
+#initals = re.search(pattern, name)
+#print((initals.group(1)+initals.group(2)+ " ") * 100)
+f_i1, m_i1, l_i1 = name.split (' ', 2)
+pattern = r'^(\w).*(\s+\w+)$'
+f_i2 = re.search(pattern, f_i1)
+m_i2 = re.search(pattern, m_i1)
+l_i2 = re.search(pattern, l_i1)
+print(f_i2+m_i2+l_i2+" "* 100)
+
+# this is printing out the first inital, but returns the full name of the other string.
+# Maybe I should split the string by spaces.
+# I think I'm discovering why every last name form asks for seperate first middle, and last names.
